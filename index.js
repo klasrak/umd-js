683f2e0c9d4d0fc069bd3c7a89f046921046c6d4
Danilo <dasf_cm@terra.com.br>
2019-05-23 20:48:56 -0300

    feat: add user model and controller

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let fields = document.querySelectorAll('#form-user-create [name]')
-let user = {}
-
-fields.forEach((field, index) => {
-  if (field.name === 'gender') {
-    if (field.checked) {
-      user[field.name] = field.value
-    }
-  } else {
-    user[field.name] = field.value
-  }
-})
-console.log(user)
+let userController = new UserController('form-user-create', 'table-users')
