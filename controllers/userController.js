53ba95c1a4f4e4ad8df9a5ad8e275149ff128eb4
Danilo <dasf_cm@terra.com.br>
2019-05-24 01:41:50 -0300

    feat: add edit form & new features

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,13 @@ class UserController {
     this.formEl = document.getElementById(formId)
     this.tableEl = document.getElementById(tableId)
     this.onSubmit()
+    this.onEdit()
+  }
+
+  onEdit () {
+    document.querySelector('#box-user-update .btn-cancel').addEventListener('click', e => {
+      this.showPanelCreate()
+    })
   }
 
   onSubmit () {
@@ -80,6 +87,7 @@ class UserController {
 
   addLine (dataUser) {
     let tr = document.createElement('tr')
+    tr.dataset.user = JSON.stringify(dataUser)
     tr.innerHTML = `      
       <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
       <td>${dataUser.name}</td>
@@ -87,10 +95,37 @@ class UserController {
       <td>${(dataUser.admin) ? 'Sim' : 'Não'}</td>
       <td>${Utils.dateFormat(dataUser.register)}</td>
       <td>
-        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
         <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
       </td>                        
     `
+    tr.querySelector('.btn-edit').addEventListener('click', e => {
+      console.log(JSON.parse(tr.dataset.user))
+      this.showPanelUpdate()
+    })
     this.tableEl.appendChild(tr)
+    this.updateCount()
+  }
+
+  showPanelCreate () {
+    document.querySelector('#box-user-create').style.display = 'block'
+    document.querySelector('#box-user-update').style.display = 'none'
+  }
+
+  showPanelUpdate () {
+    document.querySelector('#box-user-create').style.display = 'none'
+    document.querySelector('#box-user-update').style.display = 'block'
+  }
+
+  updateCount () {
+    let numberUsers = 0
+    let numberAdmin = 0;
+    [...this.tableEl.children].forEach(tr => {
+      numberUsers++
+      let user = JSON.parse(tr.dataset.user)
+      if (user._admin) { numberAdmin++ }
+    })
+    document.querySelector('#number-users').innerHTML = numberUsers
+    document.querySelector('#number-users-admin').innerHTML = numberAdmin
   }
 }
