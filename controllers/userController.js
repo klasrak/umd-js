a2c4b37f73ad32055707135aeb3ff7d4bcfa30fc
Danilo <dasf_cm@terra.com.br>
2019-05-27 15:34:38 -0300

    refactor: optimizing the template

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,19 +27,9 @@ class UserController {
         } else {
           result._photo = content
         }
-        tr.dataset.user = JSON.stringify(result)
-        tr.innerHTML = `      
-          <td><img src="${result._photo}" alt="User Image" class="img-circle img-sm"></td>
-          <td>${result._name}</td>
-          <td>${result._email}</td>
-          <td>${(result._admin) ? 'Sim' : 'Não'}</td>
-          <td>${Utils.dateFormat(result._register)}</td>
-          <td>
-            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-          </td>                        
-      `
-        this.addEventsTr(tr)
+        let user = new User()
+        user.loadFromJSON(result)
+        this.getTr(user, tr)
         this.updateCount()
         this.formUpdateEl.reset()
         btn.disabled = false
@@ -147,7 +137,13 @@ class UserController {
   }
 
   addLine (dataUser) {
-    let tr = document.createElement('tr')
+    let tr = this.getTr(dataUser)
+    this.tableEl.appendChild(tr)
+    this.updateCount()
+  }
+
+  getTr (dataUser, tr = null) {
+    if (tr === null) { tr = document.createElement('tr') }
     tr.dataset.user = JSON.stringify(dataUser)
     tr.innerHTML = `      
       <td><img src="${dataUser.photo}" alt="User Image" class="img-circle img-sm"></td>
@@ -161,8 +157,7 @@ class UserController {
       </td>                        
     `
     this.addEventsTr(tr)
-    this.tableEl.appendChild(tr)
-    this.updateCount()
+    return tr
   }
 
   addEventsTr (tr) {
